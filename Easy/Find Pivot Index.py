c2d10f9b34c07566616fca0809461399233cc45e
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:48:20 +0530

    Add Find Pivot Index (Easy) Python solution

---
+++ Easy/Find Pivot Index.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left=0
+        right=sum(nums)
+        
+        for i,n in enumerate(nums):
+            right-=n
+            if left==right:
+                return i
+            left+=n
+            
+        return -1
