9b199e69b30b264a0bf87964f5323becb2e4426c
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:52:04 +0530

    Add Construct The Rectangle (Easy) Python solution

---
+++ Easy/Construct The Rectangle.py
@@ -0,0 +1,8 @@
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+        w = int(area**0.5)
+        
+        while w>0:
+            if area%w == 0:
+                return [area//w,w]
+            w-=1
