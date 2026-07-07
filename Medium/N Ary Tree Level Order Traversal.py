d1b9272e8ccfa4e8c4783e5740e6364a231341cf
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:47:38 +0530

    Add N Ary Tree Level Order Traversal (Medium) Python solution

---
+++ Medium/N Ary Tree Level Order Traversal.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root):
+        if not root: return []
+        Q, ans = deque([root]), []
+        
+        while Q:
+            level = []
+            for i in range(len(Q)):
+                node = Q.popleft()
+                for child in node.children:
+                    Q.append(child)
+                level += [node.val] 
+            ans += [level]
+
+        return ans
