5d5a88e5e28ae72c67980eb6ff8039b52850850a
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 12:00:04 +0530

    Add Happy Number (Easy) Python solution

---
+++ Easy/Happy Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    
+    def isHappy(self, n: int) -> bool:
+        
+        def get_next(number):
+            total_sum = 0
+            while number > 0:
+                number, digit = divmod(number, 10)
+                total_sum += digit ** 2
+            return total_sum
+        
+        while n != 1 and n != 4:
+            n = get_next(n)
+            
+        return n == 1
