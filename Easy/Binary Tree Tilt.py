81de5f6e675500ce4c022d09d1799e26beabf035
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:50:45 +0530

    Add Binary Tree Tilt (Easy) Python solution

---
+++ Easy/Binary Tree Tilt.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+        self.total=0
+        
+        def dfs(root):
+            if not root:
+                return 0
+            left=dfs(root.left)
+            right=dfs(root.right)
+            tilt=abs(left-right)
+            self.total+=tilt
+            
+            return left+right+root.val
+        
+        
+        dfs(root)
+        return self.total
+        
