1603af52cc2da401b25af59149270dc10cff4356
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:42:16 +0530

    Add Smallest Integer Divisible By K (Medium) Python solution

---
+++ Medium/Smallest Integer Divisible By K.py
@@ -0,0 +1,8 @@
+class Solution:
+    def smallestRepunitDivByK(self, k: int) -> int:
+        if k%2==0 or k%5==0:
+            return -1
+        r=0
+        for n in range(1,k+1):
+            r=(r*10+1)%k
+            if not r:return n
