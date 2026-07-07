4e95d843da6a6c2602c596aa44d2f21524895db0
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:43:20 +0530

    Add Subarray Sums Divisible By K (Medium) Python solution

---
+++ Medium/Subarray Sums Divisible By K.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        res,prefix=0,0
+        
+        count=[1]+[0]*k
+        
+        for num in nums:
+            prefix=(prefix+num)%k
+            res+=count[prefix]
+            count[prefix]+=1
+            
+            
+        return res
