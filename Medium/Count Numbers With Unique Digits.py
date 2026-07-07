42e01866cbd6fa7b3b99f0fe269e99d2fdc8fe68
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:55:34 +0530

    Add Count Numbers With Unique Digits (Medium) Python solution

---
+++ Medium/Count Numbers With Unique Digits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countNumbersWithUniqueDigits(self, n: int) -> int:
+        choices=[9,9,8,7,6,5,4,3,2,1]
+        ans,product=1,1
+        for i in range(n):
+            product*=choices[i]
+            ans+=product
+            
+        return ans
