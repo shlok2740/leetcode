30ed04cf3277639eed0abce7e8e8b35b61890431
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 12:02:55 +0530

    Add Best Time To Buy And Sell Stock (Easy) Python solution

---
+++ Easy/Best Time To Buy And Sell Stock.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+
+        dp = [[0, 0] for _ in range(len(prices))]
+
+        dp[0][0],dp[0][1] = 0,-prices[0]
+
+        for i in range(1,len(prices)):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+            dp[i][1] = max(dp[i-1][1], -prices[i])
+
+        return dp[len(prices)-1][0]
+        
