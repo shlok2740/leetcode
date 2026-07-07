a4628d74eba6ea1b96d806b0ec04434a0e3b797f
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 12:04:39 +0530

    Add Restore Ip Addresses (Medium) Python solution

---
+++ Medium/Restore Ip Addresses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        res=[]
+        
+        if len(s)>12:
+            return res
+        
+        def backtrack(i,dots,curIP):
+            if dots==0 and i==len(s):
+                res.append(curIP[:-1])
+                return 
+            
+            for j in range(i,min(i+3,len(s))):
+                if int(s[i:j+1])<256 and (i==j or s[i] != "0"):
+                    backtrack(j+1,dots-1,curIP + s[i:j+1] + ".")
+                    
+        
+        backtrack(0,4,"")
+        
+        return res
