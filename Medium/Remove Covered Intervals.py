684e60a7e84bfdffe2cea105a3e17fef54f74f2c
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:20:23 +0530

    Add Remove Covered Intervals (Medium) Python solution

---
+++ Medium/Remove Covered Intervals.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        count = cur = 0
+        for _, r in sorted(intervals, key=lambda i: (i[0], -i[1])):
+            if cur < r:
+                cur = r
+                count += 1
+        return count
