612a81badb4e811e0375123f03d4a8e280566467
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:48:49 +0530

    Add Redundant Connection (Medium) Python solution

---
+++ Medium/Redundant Connection.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        tree="".join(map(chr,range(1001)))
+        for u,v in edges:
+            if tree[u]==tree[v]:
+                return [u,v]
+            tree=tree.replace(tree[u],tree[v])
