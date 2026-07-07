3741fa17311b7157a0f35d10cd1f1b1f3893152d
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:50:19 +0530

    Add Longest Harmonious Subsequence (Easy) Python solution

---
+++ Easy/Longest Harmonious Subsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        count={}
+        for x in nums:
+            count[x]=count.get(x,0)+1
+            
+        ans=0 
+        for x in count:
+            if x+1 in count:
+                ans=max(ans,count[x]+count[x+1])
+                
+        return ans
