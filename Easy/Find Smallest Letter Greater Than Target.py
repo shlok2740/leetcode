3ac96c6ffe19fc7a1e4e5d6ebf1556154e01a058
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:47:47 +0530

    Add Find Smallest Letter Greater Than Target (Easy) Python solution

---
+++ Easy/Find Smallest Letter Greater Than Target.py
@@ -0,0 +1,7 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        if letters[-1]<=target:
+            return letters[0]
+        for letter in letters:
+            if letter>target:
+                return letter
