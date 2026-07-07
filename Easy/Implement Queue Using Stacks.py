2c54da8d9c9ae96cce5de668a9c75f2406f87c18
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:58:45 +0530

    Add Implement Queue Using Stacks (Easy) Python solution

---
+++ Easy/Implement Queue Using Stacks.py
@@ -0,0 +1,44 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack=[]
+        self.size=0
+        
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.stack+=[x] 
+        self.size+=1
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        ans = self.stack[0]
+        self.stack = self.stack[1:]
+        return ans
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.stack[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not self.stack
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
