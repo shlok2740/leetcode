a68e2222933d2587e3f0699b98089dcd2af25672
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 12:04:10 +0530

    Add Symmetric Tree (Easy) Python solution

---
+++ Easy/Symmetric Tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        stack=[(root.left,root.right)]
+        while stack:
+            l,r=stack.pop()
+            if not l and not r:
+                continue
+            if not l or not r or (l.val !=r.val):
+                return False
+            stack.append((l.left,r.right))
+            stack.append((l.right,r.left))
+        return True
