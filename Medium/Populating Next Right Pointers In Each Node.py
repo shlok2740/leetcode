1b3f864a74ea54371412e932d55c5d80db7b9ad5
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 12:03:18 +0530

    Add Populating Next Right Pointers In Each Node (Medium) Python solution

---
+++ Medium/Populating Next Right Pointers In Each Node.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return
+        stack = [root]
+        while stack:
+            curr = stack.pop(0)
+            if curr.left and curr.right:
+                curr.left.next = curr.right
+                if curr.next:
+                    curr.right.next = curr.next.left
+                stack.append(curr.left)
+                stack.append(curr.right)
+        return root
