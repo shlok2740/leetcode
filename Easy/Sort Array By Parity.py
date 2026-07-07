151f4b36e6bc5995236281bc5036e0ef139afc23
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:44:31 +0530

    Add Sort Array By Parity (Easy) Python solution

---
+++ Easy/Sort Array By Parity.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        l,r=0,len(nums)-1
+        
+        while l<r:
+            
+            if nums[l]%2==0:
+                l+=1
+            else:
+                nums[l],nums[r]=nums[r],nums[l]
+                r-=1
+        
+        return nums
