41a37b1c51c79915629959457a15788ed0f187e3
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:53:24 +0530

    Add Find All Anagrams In A String (Medium) Python solution

---
+++ Medium/Find All Anagrams In A String.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        pC = collections.Counter(p)
+        res = []
+        window = None
+        for i in range(len(s)-len(p)+1):
+            if i==0: 
+                window = collections.Counter(s[i:i+len(p)])
+            else:
+                minus = {s[i-1]:1}
+                plus = {s[i+len(p)-1]:1}
+                window -=minus
+                window += plus
+            if pC==window: res.append(i)
+        return res
