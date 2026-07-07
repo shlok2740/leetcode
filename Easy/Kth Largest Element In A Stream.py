158d9f9d81e09cdc385d0f1f80bfd0542fc4b99e
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:46:59 +0530

    Add Kth Largest Element In A Stream (Easy) Python solution

---
+++ Easy/Kth Largest Element In A Stream.py
@@ -0,0 +1,18 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap=nums
+        self.value=k
+        heapq.heapify(self.heap)
+        
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap,val)
+        while len(self.heap) > self.value:
+            heapq.heappop(self.heap)
+        return self.heap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
