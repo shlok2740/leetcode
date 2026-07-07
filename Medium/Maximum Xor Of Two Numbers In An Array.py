eed5b236e02c0fb717e1e2a92836f37da6de1a1f
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:53:40 +0530

    Add Maximum Xor Of Two Numbers In An Array (Medium) Python solution

---
+++ Medium/Maximum Xor Of Two Numbers In An Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        mask,output=0,0
+        
+        for i in range(32,-1,-1):
+            mask=mask | 1<<i
+            found = set([ mask & n for n in nums])
+            
+            temp= output | 1<<i
+            for f in found:
+                if f^temp in found:
+                    output=temp
+                    
+        return output
