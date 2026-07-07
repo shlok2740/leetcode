0b045b86c4e736ccd8ab105fa0ff4e85e130b303
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 12:08:55 +0530

    Add Container With Most Water (Medium) Python solution

---
+++ Medium/Container With Most Water.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        area=0
+        
+        l,r=0,len(height)-1
+        for i in range(len(height)):
+            area=max(area,min(height[l],height[r])*(r-l))
+            
+            if height[l]<height[r]:
+                l+=1
+            else:
+                r-=1
+                
+        return area
