78a5ab6337482147a5c2156652af9651ae6bf725
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:15:29 +0530

    Add Find Valid Matrix Given Row And Column Sums (Medium) Python solution

---
+++ Medium/Find Valid Matrix Given Row And Column Sums.py
@@ -0,0 +1,14 @@
+class Solution:
+    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+        m=len(rowSum)
+        n=len(colSum)
+        
+        dp=[[0]*n for _ in range(m)]
+        
+        for i in range(m):
+            for j in range(n):
+                dp[i][j]=min(rowSum[i],colSum[j])
+                rowSum[i]-=dp[i][j]
+                colSum[j]-=dp[i][j]
+                
+        return dp
