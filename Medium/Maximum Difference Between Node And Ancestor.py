32b97fc88add5dcc640fb288f756f547c295994d
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 11:41:48 +0530

    Add Maximum Difference Between Node And Ancestor (Medium) Python solution

---
+++ Medium/Maximum Difference Between Node And Ancestor.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxAncestorDiff(self, root: TreeNode) -> int:
+        
+        self.res = 0
+        def helper(node, max_val, min_val):
+            if not node:
+                self.res = max(self.res, max_val - min_val)
+                return
+            helper(node.left, max(max_val, node.val), min(min_val, node.val))
+            helper(node.right, max(max_val, node.val), min(min_val, node.val))
+        helper(root, 0, 1000000)
+        return self.res
