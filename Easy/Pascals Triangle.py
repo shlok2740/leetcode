561bcc294ed99926430a11a591ca5e56b2a59690
Shlok Kumar <77238805+shlok2740@users.noreply.github.com>
2026-07-07 12:03:10 +0530

    Add Pascals Triangle (Easy) Python solution

---
+++ Easy/Pascals Triangle.py
@@ -0,0 +1,9 @@
+class Solution:
+    def generate(self, n: int) -> List[List[int]]:
+        res=[[1]*(i) for i in range(1,n+1)]
+        
+        for i in range(1,n):
+            for j in range(1,i):
+                res[i][j]=res[i-1][j-1]+res[i-1][j]
+                
+        return res
